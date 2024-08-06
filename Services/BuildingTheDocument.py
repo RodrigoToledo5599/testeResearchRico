6774c412159945c216d163992db6a09909ed97c9
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2024-08-05 23:45:15 -0300

    saving changes

--- Services/BuildingTheDocument.py
+++ Services/BuildingTheDocument.py
@@ -13,13 +13,18 @@ class BuildingTheDocument:
     def MontandoODocx(self):
 
         doc = Document()
-        
         dir = os.path.join(os.getcwd(),r"Output")
+        
+        doc.add_heading("Seu Guia de Investimento")
+
         print(self.data[1])
         for i in range(len(self.data)):
             doc.add_paragraph(self.table_data[i])
             doc.add_paragraph(self.data[i])
         
-        
-        
+        doc.add_paragraph()
+        outputDir = os.path.join(os.getcwd(),"/Output")
         doc.save(dir+r"\result.docx")
+        convert(dir+r"\result.docx")
+        convert(dir,r"\result.pdf")
+        convert(outputDir)
